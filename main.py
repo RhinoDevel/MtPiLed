b6599681b13d8349d52bedf1a65e826dfdd074c9
Marc <RhinoDevel@users.noreply.github.com>
2017-07-18 22:27:26 +0200

    Countdown with flashing LED. Start by push. Stop by push. Script exits after countdown.

--- main.py
+++ main.py
@@ -2,35 +2,52 @@
 
 import RPi.GPIO as GPIO
 import time
+import threading
+
+state_wait = 0
+state_init = 1
+state_done = 2
 
 pinmode = GPIO.BCM # GPIO.BOARD
 inpinnr = 27 # BCM
 outpinnr = 17 # BCM
 bouncems = 200
 
-done = False
+lock = threading.Lock()
+#
+state = state_wait
+init_seconds = 0
 
 def on_press(pinnr):
-    global done
+    global state
+    global init_seconds
     
-    done = True
-    GPIO.remove_event_detect(pinnr)
+    with lock:
+        if state == state_wait:
+    	    state = state_init
+            return
+
+        if state == state_init:
+            state = state_wait
+            init_seconds = 0
+            return
 
 def led_on_off(onseconds, offseconds):
-    print('Enabling LED..')
+    #print('Enabling LED..')
     GPIO.output(outpinnr, GPIO.HIGH)
 
-    print('Waiting '+str(onseconds)+' second(-s)..')
+    #print('Waiting '+str(onseconds)+' second(-s)..')
     time.sleep(onseconds)
 
-    print('Disabling LED..')
+    #print('Disabling LED..')
     GPIO.output(outpinnr, GPIO.LOW)
 
-    print('Waiting '+str(offseconds)+' second(-s)..')
+    #print('Waiting '+str(offseconds)+' second(-s)..')
     time.sleep(offseconds)
 
+state_copy = -1
 
-#GPIO.setwarnings(False)
+GPIO.setwarnings(False)
 GPIO.setmode(pinmode)
 GPIO.setup(outpinnr, GPIO.OUT)
 GPIO.setup(
@@ -42,18 +59,28 @@ GPIO.add_event_detect(
     callback=on_press,
     bouncetime=bouncems)
 
-#while GPIO.input(inpinnr)==0:
-while not done:
-    led_on_off(0.6, 0.4)
+while True:
+    with lock:
+        state_copy = state
 
-#print('Waiting for button release..')
-#GPIO.wait_for_edge(inpinnr, GPIO.RISING)
+    if state_copy == state_wait:
+        time.sleep(1) # Sleep for a second.
+        continue
 
-#print('Waiting for button press..')
-#GPIO.wait_for_edge(inpinnr, GPIO.FALLING)
-#time.sleep(0.2)
+    if state_copy == state_init:
+        led_on_off(0.6, 0.4) # "Sleep" for a second.
+        with lock:
+            if state == state_wait:
+                continue
+            init_seconds = init_seconds + 1
+            if init_seconds == 5:
+                state = state_done
+        continue
 
-print('Cleaning up..')
+    if state_copy == state_done:
+        break
+
+#print('Cleaning up..')
 GPIO.cleanup()
 
-print('Done.')
+#print('Done.')
